088fe29899ea77413faa5c757cb970915c55a010
Tays01 <jocetaty26@gmail.com>
2025-12-11 20:36:12 -0500

    cambios

---
+++ calculadora/app calculadora simple/app calculadora simple/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace app_calculadora_simple
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ calculadora/app calculadora simple/app calculadora simple/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app_calculadora_simple")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "app_calculadora_simple.Pages")]
